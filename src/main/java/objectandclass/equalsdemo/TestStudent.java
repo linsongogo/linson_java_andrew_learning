f2d46d3894d353d43df697e6261036496801b95e
linson <110388110+linsongogo@users.noreply.github.com>
2024-02-22 17:32:06 +0800

    equals

--- src/main/java/objectandclass/equalsdemo/TestStudent.java
+++ src/main/java/objectandclass/equalsdemo/TestStudent.java
@@ -5,8 +5,9 @@ public class TestStudent {
   public static void main(String[] args) {
     Student student1 = new Student("Andrew", "Programming");
     Student student2 = new Student("Andrew", "Coding");
+//    Student student2 = new Student("Andrew", "Programming");
 
-    System.out.println(student1.equals(student2));
+    System.out.println(student1.equals(student2));// equals方法默认指向Objetc类的equals方法，Object类的equal是判断这个变量的地址，而不是那个值本身。
   }
 
 }
